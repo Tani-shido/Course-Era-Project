ccc84a49637559a0af6f031a6cc092c8609e3a75
Tani-shido <tanishqdehariya1892004@gmail.com>
2025-10-20 22:56:51 +0530

    Preparing-React

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,7 +2,15 @@ import React from 'react'
 
 function App() {
   return (
-    <div>App</div>
+    <div>
+      <div>Navbar</div>
+      <div>Main Content</div>
+      <div>Section 1</div>
+      <div>Section 2</div>
+      <div>About</div>
+      <div>FAQ's</div>
+      <div>Footer, Contacts, etc</div>
+    </div>
   )
 }
 
