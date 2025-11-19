3788ea22d37b121ab74bf9689e4f643822e91839
Tani-shido <tanishqdehariya1892004@gmail.com>
2025-11-19 22:07:28 +0530

    Updating-Creator-Dashboard

--- Backend/Routes/updateCourse.js
+++ Backend/Routes/updateCourse.js
@@ -18,12 +18,12 @@ router.put("/update-file/:courseId", authMiddleware, creatorMiddleware, courseMi
 
 
         res.json({
-            message: "Passed! content Updated. hello"
+            message: "Passed! content Updated."
         });
     }
     catch(e){
         res.json({
-            message: "Failed! content Not Updated. hii"
+            message: "Failed! content Not Updated."
         });
     }
 
